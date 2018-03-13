73d6911433c0a86417dd15e09ad4e93c465138d0
unknown <acjordan@ucdavis.edu>
2018-03-13 15:23:40 -0700

    Mapping scripts

--- scripts/week_10 class code.R
+++ scripts/week_10 class code.R
@@ -6,3 +6,11 @@ library(rgdal)
 library(maptools)
 library(mapview)
 library(leaflet)
+library(tmap)
+
+#Install devtool package 
+install.packages("devtools")
+library(devtools)
+
+#Instal developer version of ggplot 
+devtools::install_github("tidyverse/ggplot2")
